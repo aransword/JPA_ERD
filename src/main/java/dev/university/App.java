ae9a6119179d6d611d08d1751b18f90f20d18716
hyeyoon23 <109073947+hyeyoon23@users.noreply.github.com>
2026-03-10 16:10:42 +0900

    feat: add classroom query implementation

--- src/main/java/dev/university/App.java
+++ src/main/java/dev/university/App.java
@@ -1,13 +1,25 @@
 package dev.university;
 
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.EntityManagerFactory;
+import jakarta.persistence.Persistence;
+
 /**
  * Hello world!
  *
  */
 public class App 
 {
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+    public static void main(String[] args) {
+
+        EntityManagerFactory emf =
+                Persistence.createEntityManagerFactory("hello-jpa");
+
+        EntityManager em = emf.createEntityManager();
+
+        System.out.println("DB 연결 성공");
+
+        em.close();
+        emf.close();
     }
 }
