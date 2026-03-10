fd02537e0854f72ca83648b3f3c02bf72de03207
hyun7931 <shlee4185@gmail.com>
2026-03-10 17:28:59 +0900

    feat: Insetuctor Service

--- src/main/java/dev/university/dao/InstructorDao.java
+++ src/main/java/dev/university/dao/InstructorDao.java
@@ -1,6 +1,7 @@
 package dev.university.dao;
 
 import dev.university.dto.InstructorScheduleDto;
+import dev.university.entity.Teaches;
 import jakarta.persistence.EntityManager;
 import java.util.List;
 
@@ -11,15 +12,13 @@ public class InstructorDao {
         this.em = em;
     }
 
-    public List<InstructorScheduleDto> findWeeklySchedule(String instructorId, Integer year, String semester) {
-        String jpql = "SELECT new dev.university.dto.InstructorScheduleDto(" +
-                "i.id, i.name, t.courseId, t.semester, t.year, s.building, s.roomNumber) " +
-                "FROM Teaches t " +
-                "JOIN t.instructor i " +
-                "JOIN t.section s " +
+    public List<Teaches> findWeeklySchedule(String instructorId, Integer year, String semester) {
+        String jpql = "SELECT t FROM Teaches t " +
+                "JOIN FETCH t.instructor i " +
+                "JOIN FETCH t.section s " +
                 "WHERE i.id = :id AND t.year = :year AND t.semester = :semester";
 
-        return em.createQuery(jpql, InstructorScheduleDto.class)
+        return em.createQuery(jpql, Teaches.class)
                 .setParameter("id", instructorId)
                 .setParameter("year", year)
                 .setParameter("semester", semester)
